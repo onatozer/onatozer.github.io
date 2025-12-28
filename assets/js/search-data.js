afa71e9b92f764be61ca8f7e85e506aabb726e8a
onatozer <108094560+onatozer@users.noreply.github.com>
2025-12-28 06:07:56 +0000

    Deploying to gh-pages from @ onatozer/onatozer.github.io@b56d8e4930ee6c9a9daf05b9dcae51d0c0e19698 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,15 +10,15 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-papers",
-          title: "papers",
+          title: "Papers",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/papers/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -35,26 +35,21 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-mini-alpha-go",
+          title: 'Mini-Alpha-Go',
+          description: "From scratch implementation of a 9x9 go game engine and elo-tracker, alongside an Alpha-Go implementation that uses Monte-Carlo tree search wth deep residual networks to make its next move, trained using combination of self play and supervised learning.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/mini-alphago.html";
+            },},{id: "projects-nba-monte-carlo-simulator",
+          title: 'NBA Monte-Carlo Simulator',
+          description: "Simulator that predicts the states of an NBA based off of the players on each roster trained on web-scraped data from the 22-23 regular season. Model was able to successfully predict 7 out of 8 2nd-round matchups in that following season&#39;s playoffs.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project.html";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/monte-carlo-sim.html";
+            },},{id: "projects-deepcfr-pokerbot",
+          title: 'DeepCFR Pokerbot',
+          description: "A human-level pokerbot that can play Heads Up Texas-Hodlem, built using counterfactual regret minimization with deep neural networks.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project.html";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "Mini-alpha-go",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/proj_template.html";
+              window.location.href = "/projects/poker_proj.html";
             },},{
         id: 'social-email',
         title: 'email',
